928753b33e660145abd530271cebede8be098855
Monkey Prince <kyloren3600@gmail.com>
2024-07-09 00:15:00 +0200

    initial commit

---
+++ src/main/java/com/monkeyteam/monkeyutils/MonkeyUtils.java
@@ -0,0 +1,17 @@
+package com.monkeyteam.monkeyutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MonkeyUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
